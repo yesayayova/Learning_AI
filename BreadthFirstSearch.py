026c66386afc6f2629099ddc6ec3cfb0b2d4ae98
yesayayova <49329034+yesayayova@users.noreply.github.com>
2020-10-06 15:36:01 +0700

    Simple breadth first search algorithm
    Only find an state (Without find the route)

---
+++ BreadthFirstSearch.py
@@ -0,0 +1,46 @@
+#BreadthFirstSearch.py
+#by YesayaYova
+#with Python3
+
+"""
+BREADTH FIRST SEARCH ALGORITHM
+with input is a graph
+"""
+def BFS(graph, goal, first):
+    explored = [first]
+    queue    = []
+    current  = first
+    
+    while True:        
+        #if goal was found
+        if current == goal:
+            print ("Found!")
+            break
+        
+        #check on each current's neighbour 
+        for neighbour in graph[current]:
+            if neighbour not in explored: 
+                queue.append(neighbour)
+        
+        explored.append(current)
+        current = queue.pop(0)
+        
+        #if goal wasn't found
+        if len(queue) == 0:
+            print("Not Found!")
+            break
+        
+"""
+A GRAPH TO TEST THE ALGORITHM
+"""
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+BFS(graph, "Z", "A")
+    
